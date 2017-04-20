2fc8d7df0fd362dfa03bf83eef564e27bb9a3de0
cduddy <collin.duddy@gmail.com>
2017-04-19 23:07:25 -0500

    Toggle Between Home Screens
    Added swap feature for Junk Driver Users to access both screens.

--- JunkAway/app/src/main/java/com/JunkAway/JunkDriver_HomeScreen.java
+++ JunkAway/app/src/main/java/com/JunkAway/JunkDriver_HomeScreen.java
@@ -3,18 +3,56 @@ package com.JunkAway;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class JunkDriver_HomeScreen extends AppCompatActivity {
-
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_junk_driver__home_screen);
         getSupportActionBar().hide();
         Intent i = getIntent();
-        User user = (User)i.getSerializableExtra("User");
+        user = (User)i.getSerializableExtra("User");
         TextView welcome = (TextView) findViewById(R.id.textWelcome);
         welcome.setText("Welcome, " + user.get_fname() + "! Thanks for being a Driver!");
+        Button mOpenJobs = (Button) findViewById(R.id.buttonOpenJobs);
+        mOpenJobs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //attemptLogin();
+            }
+        });
+        Button mAcceptedJobs = (Button) findViewById(R.id.buttonAcceptedJobs);
+        mAcceptedJobs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //createUser();
+            }
+        });
+
+        Button mCompletedJobs = (Button) findViewById(R.id.buttonCompletedJobs);
+        mCompletedJobs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //forgotPassword();
+            }
+        });
+        Switch mDriverModeToggle = (Switch) findViewById(R.id.switchToggleHomeScreen);
+        mDriverModeToggle.setVisibility(Switch.VISIBLE);
+        mDriverModeToggle.setChecked(true);
+        mDriverModeToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(JunkDriver_HomeScreen.this,NormalUser_HomeScreen.class);
+                intent.putExtra("User",user);
+                startActivity(intent);
+                JunkDriver_HomeScreen.this.finish();
+            }
+        });
+
     }
 }
--- JunkAway/app/src/main/java/com/JunkAway/NormalUser_HomeScreen.java
+++ JunkAway/app/src/main/java/com/JunkAway/NormalUser_HomeScreen.java
@@ -3,17 +3,57 @@ package com.JunkAway;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class NormalUser_HomeScreen extends AppCompatActivity {
-
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_user__home_screen);
         Intent i = getIntent();
-        User user = (User)i.getSerializableExtra("User");
+        user = (User)i.getSerializableExtra("User");
         TextView welcome = (TextView) findViewById(R.id.textWelcome);
         welcome.setText("Welcome, " + user.get_fname() + "!");
+        Button mCreateRequest = (Button) findViewById(R.id.buttonCreateRequest);
+        mCreateRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //attemptLogin();
+            }
+        });
+        Button mViewRequests = (Button) findViewById(R.id.buttonActiveRequests);
+        mViewRequests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               //createUser();
+            }
+        });
+
+        Button mCompletedRequests = (Button) findViewById(R.id.buttonCompleteRequests);
+        mCompletedRequests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //forgotPassword();
+            }
+        });
+        Switch mDriverModeToggle = (Switch) findViewById(R.id.switchToggleHomeScreen);
+        if(user.getJJ())
+        {
+            mDriverModeToggle.setVisibility(Switch.VISIBLE);
+            mDriverModeToggle.setChecked(false);
+        }
+        mDriverModeToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NormalUser_HomeScreen.this,JunkDriver_HomeScreen.class);
+                intent.putExtra("User",user);
+                startActivity(intent);
+                NormalUser_HomeScreen.this.finish();
+            }
+        });
     }
 }
